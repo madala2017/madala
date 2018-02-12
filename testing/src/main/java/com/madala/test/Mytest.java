b0d9dc683b5b8bad8efc0a2cc81182bb519fa52a
cimela <cimela@cimela-PC>
2018-02-12 23:56:24 +0200

    first commit

---
+++ testing/src/main/java/com/madala/test/Mytest.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.madala.test;
+
+/**
+ * @author cimela
+ *
+ */
+public class Mytest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Git testing");
+	}
+
+}
